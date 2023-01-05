9d5409c22d1c8fd7043b03820487e2d684559627
Abhinav Singh <abhinav.singh@tigeranalytics.local>
2023-01-05 12:21:05 +0530

    created a modal

--- src/app/component/notecard/notecard.component.ts
+++ src/app/component/notecard/notecard.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { UsersService } from 'src/app/users.service';
 
+declare var window: any
 @Component({
   selector: 'app-notecard',
   templateUrl: './notecard.component.html',
@@ -9,10 +10,23 @@ import { UsersService } from 'src/app/users.service';
 export class NotecardComponent implements OnInit {
   
   @Input() note: {key: string, title: string, description: string}
+  
+  formModal: any
 
   constructor(private userService: UsersService) { }
 
   ngOnInit(): void {
+    this.formModal = new window.bootstrap.Modal(
+      document.getElementById('exampleModal')
+    )
+  }
+
+  openModal(){
+    this.formModal.show()
+  }
+
+  closeModal(){
+    this.formModal.hide()
   }
   
   deleteNote(key: string){
